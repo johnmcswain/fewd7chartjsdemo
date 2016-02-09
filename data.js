96362e8a344507953d7335640f63b662d7445429
John McSwain <john.i.mcswain@gmail.com>
2016-02-08 21:29:28 -0500

    adding files

---
+++ data.js
@@ -0,0 +1,56 @@
+var MAXIMUM = 90;
+function randomNum(max){
+  return Math.floor((Math.random() * max) + 1);
+}
+
+var randomData = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [randomNum(MAXIMUM), randomNum(MAXIMUM), randomNum(MAXIMUM), randomNum(MAXIMUM), randomNum(MAXIMUM), randomNum(MAXIMUM), randomNum(MAXIMUM)]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,0,0,0.2)",
+            strokeColor: "rgba(151,0,0,1)",
+            pointColor: "rgba(151,0,0,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [randomNum(MAXIMUM), randomNum(MAXIMUM), randomNum(MAXIMUM), randomNum(MAXIMUM), randomNum(MAXIMUM), randomNum(MAXIMUM), randomNum(MAXIMUM)]
+        }
+    ]
+};
+
+var fixedData = {
+    labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 90, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 96, 27, 100]
+        }
+    ]
+};
